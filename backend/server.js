e2f76cb871089d05d832e52b8f1182adf8e7667f
Sakshin-a <81971197+Sakshin-a@users.noreply.github.com>
2023-10-02 12:16:13 +0530

    added mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 const port = 4000;
+
+mongoose.connect('mongodb://localhost/todo',{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+  });
+  const User = mongoose.model("User", userSchema);
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +26,12 @@ app.post("/register", (req, res) => {
     });
 });
 
-app.listen(port, () => {
-    console.log(`Listening at ${port}`);
-});
+
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, 'connection-error'));
+db.once('open',function(){ 
+    app.listen(port, () => {
+        console.log(`Listening at ${port}`);
+    });
+})
