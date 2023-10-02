56a450ea990cec2408de2289bbd3af82f0b12ab8
Sakshin-a <81971197+Sakshin-a@users.noreply.github.com>
2023-10-02 11:58:29 +0530

    added post register req

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 4000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", (req, res) => {
+    const {username, password} = req.body;
+    res.json({
+        username,
+        password,
+    });
 });
 
 app.listen(port, () => {
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Register() {
 
+const[username, setUserName] = useState("");
+const [password, setPassword] = useState("");
+
+const handleRegister = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:4000/register`,{
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+            username,
+            password,
+        }),
+    });
+};
+
+
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleRegister}>
         <h1>Register here!</h1>
         <br/>
-        <input placeholder="Username" />
+        <input placeholder="Username" onChange={(e) => setUserName(e.target.value)}/>
         <br/>
-        <input placeholder="Password" />
+        <input placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
         <br/>
-        <button>Register</button>
+        <button type='submit'>Register</button>
       </form>
     </div>
   )
